2ff3450d2640503b05985ceb42513cf6626c440f
Tat Sean Pang <tatsean1977@gmail.com>
2023-10-09 08:50:36 +0800

    Create hello.cs

---
+++ hello.cs
@@ -0,0 +1,11 @@
+// TODO: Need to fix something
+
+namespace HelloWorld;
+
+ internal class Program
+ {
+     static void Main(string[] args)
+     {
+         Console.WriteLine("Hello, World!");
+     }
+ }
